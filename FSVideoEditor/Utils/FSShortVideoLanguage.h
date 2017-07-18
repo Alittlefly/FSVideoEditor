15ed5a741d6837968f8b2377e7593d3f3e86c11c
wangming <wangming@feinno.com>
2017-07-18 17:59:45 +0800

    no message

---
+++ FSVideoEditor/Utils/FSShortVideoLanguage.h
@@ -0,0 +1,13 @@
+//
+//  FSShortVideoLanguage.h
+//  FSVideoEditor
+//
+//  Created by 王明 on 2017/7/18.
+//  Copyright © 2017年 Fission. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSShortVideoLanguage : NSObject
+
+@end
---
+++ FSVideoEditor/Utils/FSShortVideoLanguage.m
@@ -0,0 +1,13 @@
+//
+//  FSShortVideoLanguage.m
+//  FSVideoEditor
+//
+//  Created by 王明 on 2017/7/18.
+//  Copyright © 2017年 Fission. All rights reserved.
+//
+
+#import "FSShortVideoLanguage.h"
+
+@implementation FSShortVideoLanguage
+
+@end
