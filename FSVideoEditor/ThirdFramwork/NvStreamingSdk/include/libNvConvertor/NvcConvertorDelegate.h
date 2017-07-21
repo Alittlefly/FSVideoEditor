e1850de81c526c0cac806ddf5bfb506da2bc47f3
gaochao <1176672158@qq.com>
2017-07-21 15:15:38 +0800

    libwebp

---
+++ FSVideoEditor/ThirdFramwork/NvStreamingSdk/include/libNvConvertor/NvcConvertorDelegate.h
@@ -0,0 +1,25 @@
+//================================================================================
+//
+// (c) Copyright China Digital Video (Beijing) Limited, 2016. All rights reserved.
+//
+// This code and information is provided "as is" without warranty of any kind,
+// either expressed or implied, including but not limited to the implied
+// warranties of merchantability and/or fitness for a particular purpose.
+//
+//--------------------------------------------------------------------------------
+//   Birth Date:    June 28. 2017
+//   Author:        NewAuto video team
+//================================================================================
+#ifndef NvcConvertorDelegate_h
+#define NvcConvertorDelegate_h
+
+#import <Foundation/Foundation.h>
+
+@protocol NvcConvertorDelegate <NSObject>
+
+- (void)convertFinished;
+- (void)convertFaild:(NSError *)error;
+
+@end
+
+#endif /* NvcConvertorDelegate_h */
